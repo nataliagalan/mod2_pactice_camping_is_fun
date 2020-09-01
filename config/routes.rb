40a92da21808a32a1deee4b8b748fe1620367b29
Natalia <n8galan@gmail.com>
2020-09-01 12:08:36 -0500

    limited routes, added color to error messages, added some buttons

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :activities
-  resources :campers
-  resources :camper_activities
+  resources :activities, only: [:index, :show]
+  resources :campers, only: [:index, :show, :new, :create]
+  resources :camper_activities, only: [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
